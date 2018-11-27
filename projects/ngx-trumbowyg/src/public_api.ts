7006ca5ae447bf0900201070419b2ada97fbaa56
Alexander van Eerd <avaneerd@users.noreply.github.com>
2018-11-27 14:56:21 +0100

    Update public_api.ts

--- projects/ngx-trumbowyg/src/public_api.ts
+++ projects/ngx-trumbowyg/src/public_api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/configs/trumbowyg-options';
 export * from './lib/ngx-trumbowyg.module';
+export * from './lib/directive/editor.directive';
+export * from './lib/component/editor/editor.component';
